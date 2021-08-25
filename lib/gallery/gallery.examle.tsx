7c1a6cfe304809c5022242eca6da5b87c2b0a1c1
Tuogu <898169584@qq.com>
2021-08-25 22:36:30 +0800

    轮播组件实现跳转返回，loop循环两种方法

--- lib/gallery/gallery.examle.tsx
+++ lib/gallery/gallery.examle.tsx
@@ -3,8 +3,8 @@ import Gallery from './gallery';
 
 export default function () {
   return(
-    <div className="card" id={"slide"}>
-      <Gallery/>
+    <div className="card" >
+      <Gallery width={190} viewWidth={200} loop/>
     </div>
   )
 }
--- lib/gallery/gallery.tsx
+++ lib/gallery/gallery.tsx
@@ -1,53 +1,104 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './gallery.scss'
 
+interface Props {
+  //width移动的距离
+  width?:number
+  viewWidth?:number
+  loop?:boolean
+}
+const Gallery:React.FunctionComponent<Props>=(props)=>{
+  const [active,setActive]=useState(1)
+  const [saveActive,setSaveActive]=useState()
+  const [len,setLent]=useState()
+  // const [status,setStatus]=useState(true)
+  const view=useRef<HTMLDivElement>(null)
+  const container=useRef<HTMLUListElement>(null)
+  const dotsUl=useRef<HTMLUListElement>(null)
+  useEffect(()=>{
+    init()
+  },[])
+  const init=()=>{
+    setLent(container.current!.children.length)
+  }
+  useEffect(()=>{
+    if(props.loop){
+      setLoop()
+    }else {
+      setTransition()
+    }
+  },[active])
+  const setLoop=()=>{
+    console.log(active);
+    // @ts-ignore
+    const distance=(0-active)*props.width
+    container.current!.style.transform=`translate(${distance}px,0)`;
 
-const Gallery:React.FunctionComponent=()=>{
+    if(saveActive===1&&active===0){
+      timer(len-2)
+    }
+    if(saveActive===len-2&&active===len-1){
+      timer(1)
+    }
+
+  }
+  const timer=(a:number)=>{
+    setTimeout(()=>{
+      container.current!.style.transitionProperty="none"
+      setActive(a)
+    },500)
+    setTimeout(()=>{
+      container.current!.style.transitionProperty="all"
+    },600)
+  }
+  const setTransition=()=>{
+    console.log('loopNo');
+    if(Math.abs(saveActive-active)>1){
+      container.current!.style.transitionProperty="none"
+    }else {
+      container.current!.style.transitionProperty="all"
+    }
+    // console.log(container);
+    // @ts-ignore
+    const distance=(0-active)*props.width
+    container.current!.style.transform=`translate(${distance}px,0)`;
+
+  }
+
+
+  const handlePrev = () => {
+    setSaveActive(active)
+    setActive(active === 0 ? len-2 : active - 1)
+  }
+
+  const handleNext = () => {
+    setSaveActive(active)
+    setActive(active === len-1 ? 1 : active + 1);
+  }
+
   return (
-    <div className="re-slide-main">
-      <div className="re-slide-group">
-        <div className="re-slide-item">1</div>
-        <div className="re-slide-item">2</div>
-        <div className="re-slide-item">3</div>
+    <div className="re-slide-main" >
+      <div className="re-container" ref={view}
+           style={{width:props.viewWidth}}
+            >
+        <ul className="re-slide-group" ref={container} >
+          <li className="re-slide-item">0</li>
+          <li className="re-slide-item">1</li>
+          <li className="re-slide-item">2</li>
+          <li className="re-slide-item">3</li>
+          <li className="re-slide-item">4</li>
+        </ul>
       </div>
 
-      <ul className="re-dots">
-        <li />
+      <ul className="re-dots" ref={dotsUl}>
+        <li/>
         <li/>
         <li/>
       </ul>
 
-      <div className="slide-move-left">《</div>
-      <div className="slide-move-right">》</div>
+      <div className="slide-move-left" onClick={()=>handlePrev()}>《</div>
+      <div className="slide-move-right" onClick={()=>handleNext()}>》</div>
     </div>
   )
-
 }
-const slide=()=>{
-  // const group=document.querySelector('.re-slide-group')
-  // ReactDOM.render('group',document.querySelector('#slide'))
-}
-export {slide}
-// const div=document.createElement('div')
-// // @ts-ignore
-// document.querySelector('#slide').append(div)
-// return  ReactDOM.render(component,div)
-// // return ReactDOM.createPortal(div,document.querySelector('#slide'))
-
-
-
-
-// const item=document.querySelectorAll('.re-slide-item')
-
-// const dots=document.querySelector('.re-dots')
-//
-// const li=dots.querySelectorAll('li')
-// const moveLeft=document.querySelectorAll('.slide-move-left')
-// const moveRight=document.querySelectorAll('.slide-move-right')
-// let index=0
-// let timer=null
-
-// group.style.width=item.length+'px'
-// group.style.left=0+'px'
-
-export default  Gallery;
+export default Gallery;
